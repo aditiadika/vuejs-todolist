13854c65b2235f8d3644261bf320a66eeca9c575
dika aditia <aditiaaadicka@gmail.com>
2019-09-27 17:02:01 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NavBar />
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar";
+import AllFriends from "./components/AllFriends";
+import OnlineFriends from "./components/OnlineFriends";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar,
+    AllFriends,
+    OnlineFriends
+  },
+  data() {
+    return {
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
+    };
+  },
+  methods: {
+    deleteFriend(payload) {
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
